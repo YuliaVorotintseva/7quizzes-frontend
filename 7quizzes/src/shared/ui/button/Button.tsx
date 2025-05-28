bde2b67f2cfa46699ed0dc1c5b53942c5a35c15f
Yulia Vorotintseva <yulia.vorotintseva@gmail.com>
2025-05-28 18:10:02 +0600

    Fixed lint script and eslint errors

--- 7quizzes/src/shared/ui/button/Button.tsx
+++ 7quizzes/src/shared/ui/button/Button.tsx
@@ -2,22 +2,22 @@ import React, { MouseEventHandler } from "react";
 import "./button.css";
 
 export type Props = {
-    disabled?: false,
-    className: string,
-    onClick: MouseEventHandler<HTMLButtonElement>,
-    children: string
+  disabled?: false;
+  className: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  text: string;
 };
 
-const Button = ({ disabled, className, onClick, children }: Props) => {
-    return (
-        <button
-            className={`button ${className}`}
-            onClick={onClick}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    )
+const Button = ({ disabled, className, onClick, text }: Props) => {
+  return (
+    <button
+      className={`button ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
